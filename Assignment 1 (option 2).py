441963055efe2c4b3ae17f9f3e3f4ef12fc9a64c
arianne <arianne-lier@live.nl>
2021-02-20 20:30:02 +0100

    Implement and clean data and build model

--- Assignment 1 (option 2).py
+++ Assignment 1 (option 2).py
@@ -6,29 +6,27 @@ from sklearn.preprocessing import label_binarize
 from sklearn.tree import DecisionTreeClassifier
 
 campaign_df = pd.read_csv("campaign-data.csv")
-binarize_columns = ["children", "job", "education", "mortgage", "marital"]
-label_column = "y"
+binarize_columns = ["children", "job", "education", "mortgage", "marital", "y"]
 
 # Every row which contains the String "unknown" will get the False which is inverted with the ~ operator
 # Every value that is false will be removed from the dataset
 filtered_campaign = campaign_df[~campaign_df.eq("unknown").any(1)]
 
-# Split x (features) and y (labels) in separate dataframes
-campaign_x = filtered_campaign.copy()
-del campaign_x[label_column]
-campaign_y = filtered_campaign.copy()[[label_column]]
-
 # Binarize all categorical (string) columns
-binarized_campaign_x = pd.get_dummies(campaign_x, columns=binarize_columns)
-binarized_campaign_y = pd.get_dummies(campaign_y)
+binarized_campaign = pd.get_dummies(filtered_campaign, columns=binarize_columns)
 
-# Split dataframes into test and train with a ratio of 30%
+binarized_campaign_x = binarized_campaign.copy()
+del binarized_campaign_x["y_yes"]
+del binarized_campaign_x["y_no"]
+
+binarized_campaign_y = binarized_campaign.copy()
+binarized_campaign_y = binarized_campaign_y[["y_yes", "y_no"]]
+
 train_x, test_x, train_y, test_y = train_test_split(binarized_campaign_x, binarized_campaign_y, test_size=.3, random_state=0)
 
-# Create decision tree with max depth of 10
+# parameter minimal number of records equal to 10
 classifier = DecisionTreeClassifier(max_depth=10)
 
-# Train the decision tree model and perform a prediction with the test data
 y_score = classifier.fit(train_x, train_y).predict(test_x)
 
 fpr = dict()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 1BM110
 
-pip3 freeze > requirements.txt
+
 pip install -r requirements.txt
