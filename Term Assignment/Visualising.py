3d9159b0576037a72ca1e81173871a58f756542b
Jesses-hub <jessejagt@hotmail.com>
2021-03-04 14:08:28 +0100

    plots

--- Term Assignment/Visualising.py
+++ Term Assignment/Visualising.py
@@ -3,16 +3,18 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 
 dataset = pd.read_csv('Datasets/dataset_cleaned.csv')
-
+dataset['daily_max'] = dataset[['T1','T2','T3','T4','T5','T6','T7','T8','T9','T10','T11','T12','T13','T14','T15','T16','T17','T18','T19','T20','T21','T22','T23','T24','T25','T26','T27','T28','T29','T30','T31','T32','T33','T34','T35','T36','T37','T38','T39','T40','T41','T42','T43','T44','T45','T46','T47','T48']].max(axis=1)
 dataset['avg_consumption'] = (dataset['T1']+dataset['T2']+dataset['T3']+dataset['T4']+dataset['T5']+dataset['T6']+dataset['T7']+dataset['T8']+dataset['T9']+dataset['T10']+dataset['T11']+dataset['T12']+dataset['T13']+dataset['T14']+dataset['T15']+dataset['T16']+dataset['T17']+dataset['T18']+dataset['T19']+dataset['T20']+dataset['T21']+dataset['T22']+dataset['T23']+dataset['T24']+dataset['T25']+dataset['T26']+dataset['T27']+dataset['T28']+dataset['T29']+dataset['T30']+dataset['T31']+dataset['T32']+dataset['T33']+dataset['T34']+dataset['T35']+dataset['T36']+dataset['T37']+dataset['T38']+dataset['T39']+dataset['T40']+dataset['T41']+dataset['T42']+dataset['T43']+dataset['T44']+dataset['T45']+dataset['T46']+dataset['T47']+dataset['T48'])
 
-x = dataset.groupby(['meter_num_id','dwelling_type']).aggregate({'avg_consumption':'mean'})
+plt.figure()
+daily_max_dwelling_type = sns.boxplot(x = 'dwelling_type',y= 'daily_max',data = dataset)
 
 xlabels = ['bungalow','semi detached house','flat','detached house','terraced house']
 plt.figure()
 dwelling_type_plt = sns.boxplot(x = 'dwelling_type', y = 'avg_consumption',data = dataset)
 dwelling_type_plt = dwelling_type_plt.set_xticklabels(labels = xlabels, rotation=30)
 plt.savefig('Figures/boxplot_dwellingtype.png')
+plt.show()
 
 plt.figure()
 num_occupants_plt = sns.boxplot(x = 'num_occupants',y='avg_consumption',data=dataset)
@@ -52,3 +54,9 @@ max_temp_plt = plt.scatter(dataset['max temp'],dataset.avg_consumption,s=1)
 max_temp_plt = plt.xlabel('Maximum temperature')
 max_temp_plt = plt.ylabel('Daily consumption')
 plt.savefig('Figures/scatterplot_maxtemp.png')
+
+plt.figure()
+avg_temp_daily_max = plt.scatter(dataset['avg temp'],dataset['daily_max'],s=1)
+plt.show()
+
+#x = dataset.groupby(['meter_num_id','dwelling_type']).aggregate({'avg_consumption':'mean'})
