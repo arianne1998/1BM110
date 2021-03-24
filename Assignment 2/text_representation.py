0d6c86f2b8522eb91620ab727bf45aa7d725a71c
arianne <arianne-lier@live.nl>
2021-03-24 17:16:27 +0100

    Add Cosine similarity matrix

--- Assignment 2/text_representation.py
+++ Assignment 2/text_representation.py
@@ -2,6 +2,8 @@ import io
 
 # Requires: https://visualstudio.microsoft.com/visual-cpp-build-tools/
 import fasttext.util
+from numpy.random import random
+from sklearn.metrics.pairwise import cosine_similarity
 
 fasttext.util.download_model('en', if_exists='ignore')
 ft = fasttext.load_model('cc.en.300.bin')
@@ -21,8 +23,7 @@ for i in range(0, 3):
     print(ft.get_sentence_vector(question))
     print("\n")
 
-
-model = fasttext.train_unsupervised('Data/stackExchange-FAQ.xml')
+model = fasttext.train_unsupervised('Data/stackExchange-FAQ.xml', dim=100)
 
 # Get sentence vectors for first 3 questions
 for i in range(0, 3):
@@ -30,3 +31,15 @@ for i in range(0, 3):
     print(question)
     print(model.get_sentence_vector(question))
     print("\n")
+
+
+# Get random question from the data
+random_question_vector = model.get_sentence_vector(random.choice(questions))
+
+# Cosine similarity matrix for pre-trained
+vectors = [ft.get_sentence_vector(question) for question in questions]
+sim_matrix_pre = cosine_similarity(random_question_vector.reshape(1, -1), vectors)
+
+# Cosine similarity matrix for self trained model
+vectors = [model.get_sentence_vector(question) for question in questions]
+sim_matrix_self = cosine_similarity(random_question_vector.reshape(1, -1), vectors)
