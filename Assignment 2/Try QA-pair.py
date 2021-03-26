79ec7215a788d8a39907026dd5880d470a2d4482
sjeuken <s.jeuken@student.tue.nl>
2021-03-26 16:23:55 +0100

    QA pair dictionary

--- Assignment 2/Try QA-pair.py
+++ Assignment 2/Try QA-pair.py
@@ -17,10 +17,33 @@ for x in root.iter('qapair'):
         qapair.append(x)
         questions.append(question.text)
 
-df=pd.DataFrame({'QA-pair':qapair, 'question': questions})
-print(len(questions))
+qapair_set = set(qapair)
+qapair_list = list(qapair_set)
 
 qapair_int=[]
-for i in range(len(qapair)):
+for i in range(len(qapair_list)):
     qapair_int.append(i)
+
+dict1={}
+for i in qapair_list:
+    for k in qapair_int:
+        dict1[i]=k
+        qapair_int.remove(k)
+        break
+
+list1=list()
+for i in qapair:
+    for k in dict1.keys():
+        if k == i:
+            list1.append(dict1[k])
+
+dict_qapair={}
+for pair in list1:
+    for question in questions:
+        dict_qapair[pair]=question
+        questions.remove(question)
+        break
+
+print(dict_qapair)
+
 
